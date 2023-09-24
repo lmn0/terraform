f500a02271505a6cd83ed31d47d4cf8ba9ea65ac
Tjs <tjskrishna@gmail.com>
2023-09-24 13:47:24 -0600

    terraform studies

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "oci" {
+  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaektonwpk2cwcn35zk4gisxxh3mc2viuw5etrmehfzyzfu5h2wcqq"
+  user_ocid = "ocid1.user.oc1..aaaaaaaaugsavomcqgo4qwwhumt6qcw7prjr7rnguiwliq6due7bttfxdd5q" 
+  private_key_path = "/home/tjs/Documents/repo/terraform/private.pem"
+  fingerprint = "a5:1e:01:eb:14:a4:e2:d0:07:47:3c:09:b1:b1:b0:b5"
+  region = "us-phoenix-1"
+}
