e90e747dee3933b8d004f7ccf9c9767f47d0a7f9
Tjs <tjskrishna@gmail.com>
2025-06-24 21:05:25 -0700

    Update install_jenkins.sh

--- AWS - Terraform Samples/install_jenkins.sh
+++ AWS - Terraform Samples/install_jenkins.sh
@@ -4,5 +4,6 @@ sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo dnf upgrade
 # Add required dependencies for the jenkins package
 sudo dnf install fontconfig java-21-openjdk
-sudo dnf install jenkins
+sudo yum install java-17-amazon-corretto -y
+sudo dnf install jenkins -y
 sudo systemctl daemon-reload
