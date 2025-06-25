8776e51ac4f9ad75365eb0bd2e329ed16ffb145e
Thejass Krishnan <tjskrishna@gmail.com>
2025-06-24 18:14:12 -0700

    AWS Jenkins setup

---
+++ AWS - Terraform Samples/jenkins.tf
@@ -0,0 +1,83 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  shared_credentials_files = ["/home/tjskrishna/Documents/systems/cicd/credsf/creds"]
+}
+
+resource "aws_instance" "jenkins_ec2" {
+  ami           = "ami-0766f68f0b06ab145" 
+  instance_type = "t2.micro"
+  key_name                    = "JenkinsKeyPair"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.jenkins_security_group.id]
+  user_data                   = file("/home/tjskrishna/Documents/systems/cicd/install_jenkins.sh")
+
+  tags = {
+    Name = "Jenkins-Server"
+  }
+}
+
+resource "aws_security_group" "jenkins_security_group" {
+  name        = "jenkins_security_group"
+  description = "Allows Port SSH and HTTP Traffic"
+
+  ingress {
+    description = "Allow SSH Traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS Traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow 8080 Traffic"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "jenkins-s3-bucket-dcb"
+
+  tags = {
+    Name = "Jenkins S3 Bucket"
+  }
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+  bucket = aws_s3_bucket.my_s3_bucket.id
+  acl    = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.my_s3_bucket.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
